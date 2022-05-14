eb2be9ad4bdf5ece3068713d4de1b20d6ea81d22
eglimiraka22 <90408110+eglimiraka22@users.noreply.github.com>
2022-05-14 23:03:35 +0200

    change color

--- App.js
+++ App.js
@@ -9,29 +9,50 @@ import {
 	Linking,
 } from 'react-native';
 
-const Separator = () => <View style={styles.separator} />;
 
 const App = () => {
+	function randomHex() {
+		console.log("Color Changed");
+		let letters = "0123456789ABCDEF";
+		let color = "#";
+		for (let i = 0; i < 6; i++) {
+		  color += letters[Math.floor(Math.random() * 16)];
+		}
+		return color;
+	  }
 	const [name, setname] = useState('Egli');
 	const [session, setsession] = useState({ number: 6, title: 'state' });
+	const [current,setcurrent]=useState(true)
+	const[number ,setnumber]=useState(0)
+	const[clicktimes,setclick]=useState(0)
+	const[background,setbackground]=useState(randomHex())
 	const onClickHandler = () => {
 		setname('My name is Egli Miraka');
 		setsession({number: 7,title:'Styling text'})
+		setcurrent(false)
+		setnumber(number+5)
+		setclick(clicktimes+1)
+
 		
 	};
 	return (
-		<View style={styles.body}>
-			<Text style={styles.text}> {name}</Text>
-			<Text style={styles.text}> THis is  session number {session.number} and this session is about {session.title}</Text>
+		<View style={[styles.body,{backgroundColor:background}]}>
+			<Text style={styles.text}> {number}</Text>
+		
 
-			<Button title='Update Text' onPress={onClickHandler}></Button>
+
+			<Button title='ADD' onPress={onClickHandler}></Button>
+			<Text style={styles.text}>You clicked {clicktimes} times</Text>
+			<Button title='Change Color' onPress={() => setbackground(randomHex())}></Button>
+
+
+
 		</View>
 	);
 };
 
 const styles = StyleSheet.create({
 	body: {
-		backgroundColor: '#12ff',
 		alignItems: 'center',
 		justifyContent: 'center',
 		flex: 1,
